b7160ada151279c38239317668c6f35b76f7973a
Vincent Grimmeisen <vincegrixc@gmail.com>
2019-04-27 09:48:18 +0200

    added server File

---
+++ scripts/monitor.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat /var/log/auth.log | grep "Failed password" | awk -F 'from' '{print $2}' | cut -d" " -f2 | uniq 2>&1 > /home/vgrimmeisen/log/$(date +%d-%m-%y)_ips.txt 
+cat /var/log/auth.log | grep 'Failed password for' | awk -F 'invalid user' '{print $2}' | cut -d" " -f2 | awk 'NF' | uniq > /home/vgrimmeisen/log/$(date +%d-%m-%y)_users.txt
+# cp /var/log/auth.log /home/vgrimmeisen/log/$(date +%d-%m-%y-%H:%m)_auth.log
