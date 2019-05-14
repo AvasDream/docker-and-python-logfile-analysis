923d13bfde2446b50fe0683edb5c89d6c9ea84f6
Vincent <vincegrixc@gmail.com>
2019-05-14 12:38:17 +0200

    install script is working - be careful and use dos2unix!

--- scripts/install.sh
+++ scripts/install.sh
@@ -18,6 +18,8 @@ if [ "$?" -ne 0 ]
 then 
   echo "[!] Error while creating /data and /output"
   exit 1
+else
+  echo "[*] created /data and /output directory"
 fi
 
 chmod 744 /var/log/auth.log
@@ -25,13 +27,17 @@ if [ "$?" -ne 0 ]
 then 
   echo "[!] Error while setting the auth.log file to world readable"
   exit 1
+else
+  echo "[*] setting permissions on auth.log"
 fi
 
-docker build . -t pyauthlog
+docker build . -t pyauthlog > /dev/null
 if [ "$?" -ne 0 ]
 then 
   echo "[!] Error while building the dockerfile"
   exit 1
+else
+  echo "[*] Dockerimage build"
 fi
 
 # make monitor script executable
@@ -40,13 +46,23 @@ if [ "$?" -ne 0 ]
 then 
   echo "[!] Error while making monitor.sh script executable for everyone"
   exit 1
+else 
+  echo "[*] Making monitor.sh script executable"
 fi
 
-# Add cron job
-crontab -l > tmpcron && echo "0 3 * * *	$(pwd)/scripts/monitor.sh" >> tmpcron && crontab tmpcron && rm tmpcron
+
+crontab -l
+if [ "$?" -ne 0 ]
+then
+  echo "[*] crontab is empty creating from scratch"
+  echo "0 3 * * * $(pwd)/scripts/monitor.sh" >> tmpcron && crontab tmpcron && rm tmpcron
+else
+  echo "[*] creating crontab entry"
+  crontab -l > tmpcron && echo "0 3 * * * $(pwd)/scripts/monitor.sh" >> tmpcron && crontab tmpcron && rm tmpcron
+fi
 if [ "$?" -ne 0 ]
 then 
-  echo "[!] adding cron job"
+  echo "[!] Error while adding cron job"
   exit 1
 fi
 
